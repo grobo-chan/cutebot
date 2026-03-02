894fee263c6bcfcc1245d193277247ac2e47cf21
GroboChan <230193800+grobo-chan@users.noreply.github.com>
2026-03-02 20:31:40 +0530

    BEN PEOPLE

--- src/commands/ben.rs
+++ src/commands/ben.rs
@@ -1,13 +1,47 @@
 use crate::{Context, Error};
 use poise::serenity_prelude as serenity;
 
-/// Buns a user
+use ::serenity::all::Mentionable;
+use serenity::all::CreateEmbedAuthor;
+use serenity::builder::CreateEmbed;
+
+/// Bens one or more users
 #[poise::command(slash_command, prefix_command)]
 pub async fn ben(
     ctx: Context<'_>,
-    #[description = "Selected User"] user: serenity::User,
+    #[description = "User to ben"] users: Vec<serenity::User>,
 ) -> Result<(), Error> {
-    let response = format!("{} has been bunned!", user.name);
-    ctx.say(response).await?;
+    let embed_author =
+        CreateEmbedAuthor::new(&format!("Requested by: {}", ctx.author().display_name())).icon_url(
+            ctx.author()
+                .avatar_url()
+                .unwrap_or_else(|| ctx.author().default_avatar_url()),
+        );
+
+    let app_emojis = ctx.http().get_application_emojis().await?;
+    let loading_emoji = app_emojis
+        .iter()
+        .find(|e| e.name == "loading")
+        .map(|e| {
+            if e.animated {
+                format!("<a:{}:{}>", e.name, e.id)
+            } else {
+                format!("<:{}:{}>", e.name, e.id)
+            }
+        })
+        .unwrap_or_else(|| ":loading:".to_string());
+
+    let embed = CreateEmbed::new().author(embed_author).description(format!(
+        "**Benning:**\n {}",
+        users
+            .iter()
+            .map(|x| { format!("- {} {}", x.mention(), loading_emoji) })
+            .collect::<Vec<_>>()
+            .join("\n")
+    ));
+
+    let reply = poise::CreateReply::default().embed(embed);
+    ctx.send(reply).await?;
+
     Ok(())
 }
